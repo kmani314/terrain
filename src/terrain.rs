2c30df861fef454c59b37bc73fb00aea7745751b
Krishna Mani <krishnamani314@gmail.com>
2020-02-09 22:19:10 -0800

    Basic quad

--- src/terrain.rs
+++ src/terrain.rs
@@ -2,12 +2,13 @@ extern crate kiss3d;
 extern crate nalgebra as na;
 
 use na::Point2;
+use na::Point3;
 
 use kiss3d::window::Window;
 use kiss3d::light::Light;
 
 pub struct Terrain {
-    height_map: Vec<Vec<f32>>,
+    height_map: Vec<Point3<f32>>,
     origin: Point2<u32>,
     x_size: usize,
     y_size: usize,
@@ -16,7 +17,7 @@ pub struct Terrain {
 impl Terrain {
     pub fn new(origin: Point2<u32>, x_size: usize, y_size: usize) -> Terrain {
         Terrain {
-            height_map: vec![vec![0f32; x_size]; y_size],
+            height_map: Vec::new(), 
             origin: origin,
             x_size: x_size,
             y_size: y_size,
@@ -29,6 +30,11 @@ impl Terrain {
 
     pub fn render(&self) {
         let mut window = Window::new("terrain");
+        let mut quad = window.add_quad(5f32, 5f32, self.x_size, self.y_size);
+
+        window.set_light(Light::StickToCamera);
+
+        quad.set_color(1f32, 1f32, 1f32); 
         while window.render() {}
     }
 }
